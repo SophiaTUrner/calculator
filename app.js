fdf9391a9b23116b71b42668c59ce77d67ee0c6f
SophiaTUrner <turnersophia220@gmail.com>
2024-07-22 16:49:44 +0000

    add

--- app.js
+++ app.js
@@ -11,4 +11,36 @@ let decimalAdded = false
 
 const operators = ["+", "-", "x", "/"]
 
-function handleKeyPress(e){}
+function handleKeyPress(e){
+    const key = e.target.dataset.key
+    const lastChar = operation[operation.length -1]
+
+    if (key === "="){
+        return
+    }
+
+    if(key === "." && decimalAdded){
+        return
+    }
+
+    if(operators.indexOf(key)!== -1){
+        decimalAdded = false
+    }
+
+    if (operation.length === 0 && key === "-"){
+        operation += key
+        input.innerHTML = operation
+        return
+    }
+    if(operation.length === 0 && operators.indexOf(key) !== -1){
+        input.innerHTML = operation
+        return
+
+    }
+
+    if (operators.indexOf(lastChar) !== -1 && operators.indexOf(key) !== -1){
+        operation = operation.replace(/.$/, key)
+        input.innerHTML = operation
+        return
+    }
+}
